f33b16ee4506095a47893510e01850d9cd14e19f
jiseungyoep <91867790+jiseungyoep@users.noreply.github.com>
2021-10-06 10:52:29 +0900

    Update PlayConn.java

--- PlayConn.java
+++ PlayConn.java
@@ -5,16 +5,38 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class PlayerConn {
-	public static Connection getConn() throws ClassNotFoundException, SQLException{
-		//이클립스 db 연결용.......
+	public static Connection getConn2019() throws ClassNotFoundException, SQLException{
+		//2019년도 db 접속용
 	
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		
 		String user = "root";
 		String password = "mysql";
-		String url =  "jdbc:mysql://localhost:3306/kt_wiz?characterEncoding=utf-8&&serverTimezone=Asia/Seoul";
+		String url =  "jdbc:mysql://localhost:3306/ktwiz2019?characterEncoding=utf-8&&serverTimezone=Asia/Seoul";
 		
 		return DriverManager.getConnection(url, user,password);
 	}
-}
 	
+	public static Connection getConn2020() throws ClassNotFoundException, SQLException{
+		//2020년도 db 접속용
+	
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		
+		String user = "root";
+		String password = "mysql";
+		String url =  "jdbc:mysql://localhost:3306/ktwiz2020?characterEncoding=utf-8&&serverTimezone=Asia/Seoul";
+		
+		return DriverManager.getConnection(url, user,password);
+	}
+	public static Connection getConn2021() throws ClassNotFoundException, SQLException{
+		//2021년도 db 접속용
+	
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		
+		String user = "root";
+		String password = "mysql";
+		String url =  "jdbc:mysql://localhost:3306/ktwiz2021?characterEncoding=utf-8&&serverTimezone=Asia/Seoul";
+		
+		return DriverManager.getConnection(url, user,password);
+	}
+}	
