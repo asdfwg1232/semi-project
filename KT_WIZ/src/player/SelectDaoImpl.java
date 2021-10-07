1591ad1299cdeff7f350ad79e749892971306af5
sinup <sinup@DESKTOP-RH6APPV>
2021-10-07 10:14:22 +0900

    수정

--- KT_WIZ/src/player/SelectDaoImpl.java
+++ KT_WIZ/src/player/SelectDaoImpl.java
@@ -18,7 +18,7 @@ public class SelectDaoImpl implements SelectDao{
 	@Override//투수 컬럼으로 조회
 	//name, salary, deposit, position, number, era, phr, win, lose, sv
 	public List<Pitcherplayer> pitcherOrderBy(String columnName, String orderBy) throws ClassNotFoundException, SQLException {
-		String sql ="select * from Pitcher order by "+columnName+" "+orderBy;
+		String sql ="select * from pitcher order by "+columnName+" "+orderBy;
 		try(Connection conn = PlayerConn.getConn2021();
 				Statement stmt =conn.createStatement();
 				ResultSet rs = stmt.executeQuery(sql)){
@@ -38,7 +38,7 @@ public class SelectDaoImpl implements SelectDao{
 	@Override//타자 컬럼으로 조회
 	//name, salary, deposit, position, number,avg, hr,sb
 	public List<Batterplayer> batterOrderBy(String columnName, String orderBy) throws ClassNotFoundException, SQLException {
-		String sql ="select * from Batter order by "+columnName+" "+orderBy;
+		String sql ="select * from batter order by "+columnName+" "+orderBy;
 		try(Connection conn = PlayerConn.getConn2021();
 				Statement stmt =conn.createStatement();
 				ResultSet rs = stmt.executeQuery(sql)){
@@ -55,7 +55,7 @@ public class SelectDaoImpl implements SelectDao{
 	
 	@Override//투수 개인 조회
 	public List<Pitcherplayer> pitcherFindLimit(String columnName, String orderBy, int num) throws ClassNotFoundException, SQLException{
-		String sql ="select * from Pitcher "+columnName+" order by "+columnName+" "+orderBy+" limit "+num;
+		String sql ="select * from pitcher "+columnName+" order by "+columnName+" "+orderBy+" limit "+num;
 		try(Connection conn = PlayerConn.getConn2021();
 				Statement stmt =conn.createStatement();
 				ResultSet rs = stmt.executeQuery(sql)){
@@ -74,7 +74,7 @@ public class SelectDaoImpl implements SelectDao{
 	
 	@Override//타자 개인 조회
 	public List<Batterplayer> batterFindLimit(String columnName, String orderBy, int num) throws ClassNotFoundException, SQLException {
-		String sql ="select * from Batter "+columnName+" order by "+columnName+" "+orderBy+" limit "+num;
+		String sql ="select * from batter "+columnName+" order by "+columnName+" "+orderBy+" limit "+num;
 		try(Connection conn = PlayerConn.getConn2021();
 				Statement stmt =conn.createStatement();
 				ResultSet rs = stmt.executeQuery(sql)){
@@ -90,7 +90,7 @@ public class SelectDaoImpl implements SelectDao{
 	}
 	@Override//투수 연봉 top 5명 조회
 	public List<Pitcherplayer> pSalaryLimit(String orderBy) throws ClassNotFoundException, SQLException {
-		String sql = "select * from Pitcherplayer order by salary "+orderBy+ " limit 5";
+		String sql = "select * from pitcher order by salary "+orderBy+ " limit 5";
 		try(Connection conn = PlayerConn.getConn2021();
 				Statement stmt =conn.createStatement();
 				ResultSet rs = stmt.executeQuery(sql)){
@@ -106,7 +106,7 @@ public class SelectDaoImpl implements SelectDao{
 	}
 	@Override//타자 연봉 top 10명 조회
 	public List<Batterplayer> bSalaryLimit(String orderBy) throws ClassNotFoundException, SQLException {
-		String sql = "select * from Batterplayer order by salary "+orderBy+" limit 10";
+		String sql = "select * from batter order by salary "+orderBy+" limit 10";
 		try(Connection conn = PlayerConn.getConn2021();
 				Statement stmt =conn.createStatement();
 				ResultSet rs = stmt.executeQuery(sql)){
