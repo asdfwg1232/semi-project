bd681819a85c3f642e4120fd616be66b31b43a69
jiseungyoep <91867790+jiseungyoep@users.noreply.github.com>
2021-10-08 11:08:23 +0900

    Update and rename MAIN.java to Main1.java
    read / write 추가 / jre 버전 16

--- insert,update,delete모음/KT_WIZ/src/player/Main1.java
+++ insert,update,delete모음/KT_WIZ/src/player/Main1.java
@@ -1,12 +1,18 @@
 package player;
 
+import java.io.File;
+import java.io.IOException;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Scanner;
 
-public class MAIN {
+
+
+public class Main1 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		PlayerDao dao = PlayerImp.getInst();
+		PlayerDao dao = PlayerImp.getInstance();
+	
 		
 		while(true) {
 			System.out.println("무엇을 하시겠습니까?");
@@ -14,42 +20,41 @@ public class MAIN {
 			System.out.println("2. 선수 정보 삭제하기");
 			System.out.println("3. 선수 성적 수정하기");
 			System.out.println("4. 선수 연봉 수정하기");
-			System.out.println("5. 종료");
+			System.out.println("5. 선수 정보 test파일에 저장하기");
+			System.out.println("6. 선수 정보 test파일 읽어오기");
+			System.out.println("7. 종료");
 			String choice = scan.next();
 			
 			switch(choice) {
 			case "1" :
 				System.out.println("입력할 선수의 포지션은?(투수 or 타자)");
 				String position = scan.next();
-
+				
 				if(position.equals("투수")) {
 					while(true) {
-
-						System.out.println("이름 입력");
+						
+						System.out.println("name, salary, depoist, number");
 						String name = scan.next();
-						System.out.println("연봉 입력 (단위 : 만원)");
 						int salary = scan.nextInt();
-						System.out.println("계약금 입력 (단위 : 만원");
 						int deposit = scan.nextInt();
-						System.out.println("등번호 입력");
 						int number = scan.nextInt();
-						System.out.println("평균자책점 입력");
+						System.out.println("era입력");
 						double era = scan.nextDouble();
-						System.out.println("피홈런 수 입력");
+						System.out.println(" phr");
 						int phr = scan.nextInt();
-						System.out.println("승 수 입력");
+						System.out.println("win");
 						int win = scan.nextInt();
-						System.out.println("패 수 입력");
+						System.out.println("lose입력");
 						int lose = scan.nextInt();
-						System.out.println("세이브 수 입력");
+						System.out.println("sv 입력");
 						int sv = scan.nextInt();
 						
 						Pitcherplayer pitcher = new Pitcherplayer(name, salary, deposit, position, number, era, phr, win, lose, sv);
 						try {
-
+							
 							int a = dao.insertPitcher(pitcher);
 							System.out.println(a + " insert complete");
-
+							
 						} catch (Exception e) {
 							// TODO: handle exception
 						}		
@@ -59,46 +64,41 @@ public class MAIN {
 						if(str.equals("n")) {
 							break;
 						}
-				}
+					}
 				}
 //			-------------------------------------------------------------------------------------
 				
 				if(position.equals("타자")) {
 					
 					while(true) {
-						System.out.println("이름 입력");
+						System.out.println("name, salary, depoist, number");
 						String name1 = scan.next();
-						System.out.println("연봉 입력");
 						int salary1= scan.nextInt();
-						System.out.println("계약금 입력");
 						int deposit1 = scan.nextInt();
-						System.out.println("등번호 입력");
 						int number1 = scan.nextInt();
-						System.out.println("타율 입력");
+						System.out.println("avg, hr, sb 입력");
 						double avg = scan.nextDouble();
-						System.out.println("홈런 수 입력");
 						int hr = scan.nextInt();
-						System.out.println("도루 수 입력");
 						int sb = scan.nextInt();
-				
+						
 						Batterplayer batter = new Batterplayer(name1, salary1, deposit1, position, number1, avg, hr, sb);
-
+						
 						try {
 							
 							int a1 = dao.insertBatter(batter);
 							System.out.println(a1 + " insert complete");
-
+							
 						} catch (Exception e) {
 							// TODO: handle exception
 						}		
-
+						
 						System.out.println("계속 하시겠습니까? y or n");
 						String str2 = scan.next();
 						if(str2.equals("n")) {
 							break;
 						}
 					}
-						
+					
 				}
 				break;
 			case "2" :
@@ -130,7 +130,7 @@ public class MAIN {
 						}
 					}
 //					------------------------------------------
-			       if(posi.equals("타자")) {
+					if(posi.equals("타자")) {
 						
 						try {
 							
@@ -151,13 +151,13 @@ public class MAIN {
 						}
 					}
 					
-			       System.out.println("계속 하시겠습니까? y or n");
-			       String yn = scan.next();
-			       if(yn.equals("n")) {
-			    	   break;
-			       }
-					
+					System.out.println("계속 하시겠습니까? y or n");
+					String yn = scan.next();
+					if(yn.equals("n")) {
+						break;
 					}
+					
+				}
 				break;
 			case "3" :
 				
@@ -166,22 +166,19 @@ public class MAIN {
 					String position2 = scan.next();
 					System.out.println("수정하고싶은 선수 이름은?");
 					String pname = scan.next();
-
+					
 					if(position2.equals("타자")) {
 						try {
-							System.out.println("수정할 타율을 입력하세요");
+							System.out.println("수정하고싶은 avg,hr,sb의 값을 입력하세요");
 							double avg1 = scan.nextDouble();
-							System.out.println("수정 할 홈런 수를 입력하세요");
 							int hr1 = scan.nextInt();
-							System.out.println("수정 할 도루 수를 입력하세요");
 							int sb1 = scan.nextInt();
-							
 							int result = dao.updateBatter(pname, avg1, hr1, sb1);
-
+							
 							if(result>0) {
 								System.out.println("update complete");
 							}
-
+							
 						} catch (Exception e) {
 							// TODO: handle exception
 						}
@@ -189,19 +186,15 @@ public class MAIN {
 //					-----------------------------------------------------------
 					
 					if(position2.equals("투수")) {
-
+						
 						try {
-							System.out.println("수정 할 평균자책점을 입력하세요");
+							System.out.println("수정하고싶은 era,phr,win,lose,sv 의 값을 입력하세요");
 							double ear1 = scan.nextDouble();
-							System.out.println("수정 할 피홈런 수를 입력하세요");
 							int phr1 = scan.nextInt();
-							System.out.println("수정 할 승 수를 입력하세요");
 							int win1 = scan.nextInt();
-							System.out.println("수정 할 패 수를 입력하세요");
 							int lose1 = scan.nextInt();
-							System.out.println("수정 할 세이브 수를 입력하세요");
 							int sv1 = scan.nextInt();
-
+							
 							int result1 = dao.Pitcherupdate(pname, ear1, phr1, win1, lose1, sv1);
 							if(result1>0) {
 								System.out.println("update complete");
@@ -209,17 +202,17 @@ public class MAIN {
 						} catch (Exception e) {
 							// TODO: handle exception
 						}
-
+						
 					}
-
+					
 					System.out.println("계속 하시겠습니까? y or n");
 					String str = scan.next();
 					if(str.equals("n")) {
 						break;
 					}
 				}
-			break;
-			
+				break;
+				
 			case "4" :
 				
 				System.out.println("연봉 인상 할 선수의 포지션을 고르세요");
@@ -233,7 +226,7 @@ public class MAIN {
 					double avg = scan.nextDouble();
 					
 					
-					System.out.println("기존연봉에 인상할 연봉을 적어주세요 (단위:만원");
+					System.out.println("기존연봉에 인상할 연봉을 적어주세요");
 					int salary = scan.nextInt();
 					
 					
@@ -257,10 +250,10 @@ public class MAIN {
 					double era = scan.nextDouble();
 					
 					
-					System.out.println("기존연봉에 인상할 연봉을 적어주세요 (단위:만원)");
+					System.out.println("기존연봉에 인상할 연봉을 적어주세요");
 					int salary = scan.nextInt();
 					
-
+					
 					try {
 						Pitcherplayer player = new Pitcherplayer();
 						player.setSalary(salary);
@@ -278,18 +271,97 @@ public class MAIN {
 					}
 				}
 				
-
 				
-			
-		}
-			
+				
+				
+			case "5" :	
+				
+				System.out.println("타자 or 투수 중 어느걸 파일에 저장할지 골라주세요.");
+				String str5 = scan.next();
+				System.out.println("선수정보를 저장하실 파일명을 입력해주세요.");
+				String filename = scan.next();
+				String filepath = "c:\\test\\";
+				if(str5.equals("타자")) {
+					try {
+						List<Batterplayer> playerList = dao.PlayerYears_batterFindAll();
+						for(Batterplayer player : playerList) {
+						}
+						dao.writeBatterplayerFile(filepath, filename, playerList);
+						System.out.println("타자 관련 파일을 작성 완료했습니다.");
+					}catch(ClassNotFoundException e) {
+						e.printStackTrace();
+					}catch(SQLException e) {
+						e.printStackTrace();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}else if(str5.equals("투수")) {
+					try {
+						List<Pitcherplayer> playerList = dao.PlayerYars_pitcherFindAll();
+						for(Pitcherplayer player : playerList) {
+						}
+						dao.writePitcherplayerFile(filepath, filename, playerList);
+						System.out.println("투수 관련 파일을 작성 완료했습니다.");
+					}catch(ClassNotFoundException e) {
+						e.printStackTrace();
+					}catch(SQLException e) {
+						e.printStackTrace();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+				break;
+				
+				
+			case "6" :
+				PlayerImp pi= new PlayerImp();
+				Batterplayer bp = new Batterplayer();
+				Pitcherplayer pp = new Pitcherplayer();
+				System.out.println("선수정보가 저장된 파일명을 입력해주세요.");
+				String filename1 = scan.next();
+				String filepath1 = "c:\\test\\";
+				
+    			System.out.println("해당 파일이 타자 or 투수 관련 파일인가요?");
+				String ans = scan.next();
+				while(true) {
+					if(ans.equals("타자")) {
+						try {
+							pi.setInfoList_Batter(dao.readFile_batter(new File(filepath1, filename1)));
+							System.out.println(pi.infoList_batter+"\n");
+						}catch(ClassNotFoundException e) {
+							e.printStackTrace();
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						} catch (SQLException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}				
+					}else if(ans.equals("투수")) {				
+							try {
+								pi.setInfoList_pitcher(dao.readFile_pitcher(new File(filepath1, filename1)));
+								System.out.println(pi.infoList_pitcher+"\n");
+							}catch(ClassNotFoundException e) {
+								e.printStackTrace();
+							} catch (IOException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							} catch (SQLException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}	
+					}
+					break;
+				}
+			}
 
-		if(choice.equals("5")) {
 			
-			break;
+			if(choice.equals("7")) {
+				
+				break;
+			}
+			
 		}
 		
 	}
-
-}
 }
